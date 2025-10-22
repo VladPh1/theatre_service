99bfe32c6713c634975832212b3939c14b274d8f
Vladyslav Pihol <freepgvlad@gmail.com>
2025-10-22 16:53:44 +0300

    add changes in settings.py & add to theatre/models.py model Ticket

--- theatre/models.py
+++ theatre/models.py
@@ -1,4 +1,8 @@
+from django.core.exceptions import ValidationError
 from django.db import models
+
+from theatre_service import settings
+
 
 class Play(models.Model):
     title = models.CharField(max_length=255)
@@ -28,7 +32,6 @@ class Actor(models.Model):
 #     filename = (f"{slugify(movie.title)}-{str(uuid.uuid4())}"
 #                 + pathlib.Path(filename).suffix)
 #     return str(pathlib.Path("uploads/movies/") / pathlib.Path(filename))
-
 
 
 class TheatreHall(models.Model):
@@ -64,3 +67,60 @@ class Reservation(models.Model):
 
     class Meta:
         ordering = ["-created_at"]
+
+
+class Ticket(models.Model):
+    performance = models.ForeignKey(
+        Performance, on_delete=models.CASCADE, related_name="tickets"
+    )
+    reservation = models.ForeignKey(
+        Reservation, on_delete=models.CASCADE, related_name="tickets"
+    )
+    row = models.IntegerField()
+    seat = models.IntegerField()
+
+    @staticmethod
+    def validate_ticket(row, seat, theatre_hall, error_to_raise):
+        for ticket_attr_value, ticket_attr_name, theatre_hall_attr_name in [
+            (row, "row", "rows"),
+            (seat, "seat", "seats_in_row"),
+        ]:
+            count_attrs = getattr(theatre_hall, theatre_hall_attr_name)
+            if not (1 <= ticket_attr_value <= count_attrs):
+                raise error_to_raise(
+                    {
+                        ticket_attr_name: f"{ticket_attr_name} "
+                        f"number must be in available range: "
+                        f"(1, {theatre_hall_attr_name}): "
+                        f"(1, {count_attrs})"
+                    }
+                )
+
+    def clean(self):
+        Ticket.validate_ticket(
+            self.row,
+            self.seat,
+            self.performance.theatre_hall,
+            ValidationError,
+        )
+
+    def save(
+        self,
+        force_insert=False,
+        force_update=False,
+        using=None,
+        update_fields=None,
+    ):
+        self.full_clean()
+        return super(Ticket, self).save(
+            force_insert, force_update, using, update_fields
+        )
+
+    def __str__(self):
+        return (
+            f"{str(self.performance)} (row: {self.row}, seat: {self.seat})"
+        )
+
+    class Meta:
+        unique_together = ("performance", "row", "seat")
+        ordering = ["row", "seat"]
