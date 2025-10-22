5bc214eba77e52dfe32695f480a9bef45c3199c7
Vladyslav Pihol <freepgvlad@gmail.com>
2025-10-22 22:05:12 +0300

    remove .flake8 & db.sqlite3 & identifier.sqlite and fixed errors with migrations

--- theatre_service/settings.py
+++ theatre_service/settings.py
@@ -131,3 +131,7 @@ MEDIA_URL = "/media/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
--- user/models.py
+++ user/models.py
@@ -43,6 +43,8 @@ class UserManager(DjangoUserManager):
 class User(AbstractUser):
     """User model."""
 
+    username = None
+
     email = models.EmailField(_("email address"), unique=True)
 
     USERNAME_FIELD = "email"
