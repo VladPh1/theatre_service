ba755fb250e09bcbd61c06acb863ff0119874de1
Vladyslav Pihol <freepgvlad@gmail.com>
2025-10-22 17:31:42 +0300

    add totheatre/serializers.py serializers GenreSerializer & ActorSerializer & TheatreHallSerializer & PlaySerializer & PerformanceSerializer & TicketSerializer & ReservationSerializer

--- theatre/serializers.py
+++ theatre/serializers.py
@@ -10,3 +10,63 @@ from theatre.models import (
     Ticket,
     Reservation,
 )
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ("id", "name")
+
+
+class ActorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = ("id", "first_name", "last_name", "full_name")
+
+
+class TheatreHallSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TheatreHall
+        fields = ("id", "name", "rows", "seats_in_row", "capacity")
+
+
+class PlaySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Play
+        fields = ("id", "title", "description")
+
+
+class PerformanceSerializer(serializers.ModelSerializer):
+    plays = PlaySerializer(many=True)
+    theatre_halls = TheatreHallSerializer(many=True)
+    class Meta:
+        model = Performance
+        fields = ("id", "plays", "theatre_halls", "show_time")
+
+
+class TicketSerializer(serializers.ModelSerializer):
+    def validate(self, attrs):
+        data = super(TicketSerializer, self).validate(attrs=attrs)
+        Ticket.validate_ticket(
+            attrs["row"], attrs["seat"], attrs["performance"]
+        )
+        return data
+
+    class Meta:
+        model = Ticket
+        fields = ("id", "row", "seat", "performance")
+
+
+class ReservationSerializer(serializers.ModelSerializer):
+    tickets = TicketSerializer(many=True, read_only=False, allow_empty=False)
+    class Meta:
+        model = Reservation
+        fields = ("id", "tickets", "created_at")
+
+    def create(self, validated_data):
+        with transaction.atomic():
+            tickets_data = validated_data.pop("tickets")
+            reservation = Reservation.objects.create(**validated_data)
+            for ticket_data in tickets_data:
+                Ticket.objects.create(reservation=reservation, **ticket_data)
+            return reservation
