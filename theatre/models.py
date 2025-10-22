246e3ab4314ec257f7d52db7d5de972cee396a93
Vladyslav Pihol <freepgvlad@gmail.com>
2025-10-22 16:19:52 +0300

    add to theatre/models.py model Play & Genre & Actor

--- theatre/models.py
+++ theatre/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class Play(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+# def movie_image_path(movie: "Movie", filename: str) -> str:
+#     filename = (f"{slugify(movie.title)}-{str(uuid.uuid4())}"
+#                 + pathlib.Path(filename).suffix)
+#     return str(pathlib.Path("uploads/movies/") / pathlib.Path(filename))
